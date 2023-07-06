409c953c34b6dc490155d65bab0c77fb5a70a73a
OxytToledo <gabriel.jtoledouv@gmail.com>
2023-07-06 10:38:06 -0300

    Feat: inserir layout Header.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import Header from './presentation/templates/Header';
+import Main from './presentation/templates/Main';
+
+import { useGetCharactersStarWarsFromAPI } from './services/charactersStarWars.service';
+
 function App() {
+  useGetCharactersStarWarsFromAPI()
+  
   return (
     <div className="app">
-    
+      <Header/>
+      <Main/>
     </div>
   );
 }
--- frontend/src/presentation/templates/Header.tsx
+++ frontend/src/presentation/templates/Header.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <header className="">
-    
+    <header className="flex justify-center bg-secondary-100 p-3">
+      <div className="max-w-5xl w-full">
+        <Link to={`/`} className='text-primary text-3xl'>Star Wars Academy</Link>
+      </div>
     </header>
   )
 }
