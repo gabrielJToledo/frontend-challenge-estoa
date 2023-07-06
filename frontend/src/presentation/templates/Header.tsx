880394f281df1bc29ff966b995ed5b7e6a1b7f51
OxytToledo <gabriel.jtoledouv@gmail.com>
2023-07-06 13:29:09 -0300

    Feat: inserir font starWars Header.tsx

--- frontend/src/presentation/templates/Header.tsx
+++ frontend/src/presentation/templates/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="flex justify-center bg-secondary-100 p-3">
       <div className="max-w-5xl w-full">
-        <Link to={`/`} className='text-primary text-3xl'>Star Wars Academy</Link>
+        <Link to={`/`} className='text-primary starWarsFont text-3xl'>Star Wars Academy</Link>
       </div>
     </header>
   )
