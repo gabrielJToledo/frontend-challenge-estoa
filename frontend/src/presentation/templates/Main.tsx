9ca9377df8f7117e139a221df42526b31bc58979
OxytToledo <gabriel.jtoledouv@gmail.com>
2023-07-06 11:18:44 -0300

    Feat: adicionar Character component no Main.tsx

--- frontend/src/presentation/templates/Main.tsx
+++ frontend/src/presentation/templates/Main.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from '../home/Home';
+import Character from '../character/Character';
 
 function Main() {
   return (
     <main className="bg-secondary-200 flex justify-center p-4">
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/personagem/:id' element={<Character/>}/>
       </Routes>
     </main>
   );
