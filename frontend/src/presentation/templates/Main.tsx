37bb7a7ac504efb77c671db48ed23d4aa98af5c2
OxytToledo <gabriel.jtoledouv@gmail.com>
2023-07-06 10:44:21 -0300

    Feat: inserir browserRouter e Provider index.tsx

--- frontend/src/presentation/templates/Main.tsx
+++ frontend/src/presentation/templates/Main.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from '../home/Home';
 
 function Main() {
   return (
-    <main className="">
-
+    <main className="bg-secondary-200 flex justify-center p-4">
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </main>
   );
 }
